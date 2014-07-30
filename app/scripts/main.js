a5e659baeaa2f3eb2e208b0dfe92395a4e51bff5
Connor Hoehn <connor@inclindinc.com>
2014-07-29 21:27:53 -0400

    Initial commit.

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global OceanviewlivingInfo, $*/
+
+
+window.OceanviewlivingInfo = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    OceanviewlivingInfo.init();
+});
